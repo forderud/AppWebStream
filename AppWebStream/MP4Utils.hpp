fbac71fa75877bf233912cfbe9c5d75e024548f5
Fredrik Orderud <forderud@gmail.com>
2025-05-04 13:07:57 +0200

    Move MP4 serialization utility functions to a separate header.

---
+++ AppWebStream/MP4Utils.hpp
@@ -0,0 +1,142 @@
+#pragma once
+#include <Windows.h>
+
+
+/** Deserialize & conververt from big-endian. */
+template <typename T>
+static T DeSerialize(const char* buf) {
+    T val = {};
+    for (size_t i = 0; i < sizeof(T); ++i)
+        reinterpret_cast<BYTE*>(&val)[i] = buf[sizeof(T) - 1 - i];
+
+    return val;
+}
+
+/** Serialize & conververt to big-endian. */
+template <typename T>
+static char* Serialize(char* buf, T val) {
+    for (size_t i = 0; i < sizeof(T); ++i)
+        buf[i] = reinterpret_cast<BYTE*>(&val)[sizeof(T) - 1 - i];
+
+    return buf + sizeof(T);
+}
+
+/** Read big-endian fixed-point 16+16 float.
+    REF: https://github.com/sannies/mp4parser/blob/master/isoparser/src/main/java/org/mp4parser/tools/IsoTypeReader.java */
+inline double ReadFixed1616(const char* buf) {
+    int32_t val = 0;
+    val |= buf[0] << 24;
+    val |= buf[1] << 16;
+    val |= buf[2] << 8;
+    val |= buf[3] << 0;
+
+    return ((double)val) / (1 << 16);
+}
+/** Read big-endian fixed-point 2+30 float.
+    REF: https://github.com/sannies/mp4parser/blob/master/isoparser/src/main/java/org/mp4parser/tools/IsoTypeReader.java */
+inline double ReadFixed0230(const char* buf) {
+    int32_t val = 0;
+    val |= buf[0] << 24;
+    val |= buf[1] << 16;
+    val |= buf[2] << 8;
+    val |= buf[3] << 0;
+
+    return ((double)val) / (1 << 30);
+}
+
+/** Write big-endian fixed-point 16+16 float.
+    REF: https://github.com/sannies/mp4parser/blob/master/isoparser/src/main/java/org/mp4parser/tools/IsoTypeWriter.java */
+inline char* WriteFixed1616(char* buf, double in) {
+    int32_t val = (int32_t)(in * (1 << 16));
+
+    buf[0] = (val & 0xFF000000) >> 24;
+    buf[1] = (char)((val & 0x00FF0000) >> 16);
+    buf[2] = (val & 0x0000FF00) >> 8;
+    buf[3] = (val & 0x000000FF);
+    return buf + 4;
+}
+/** Write big-endian fixed-point 2+30 float.
+    REF: https://github.com/sannies/mp4parser/blob/master/isoparser/src/main/java/org/mp4parser/tools/IsoTypeWriter.java */
+inline char* WriteFixed0230(char* buf, double in) {
+    int32_t val = (int32_t)(in * (1 << 30));
+
+    buf[0] = (val & 0xFF000000) >> 24;
+    buf[1] = (char)((val & 0x00FF0000) >> 16);
+    buf[2] = (val & 0x0000FF00) >> 8;
+    buf[3] = (val & 0x000000FF);
+    return buf + 4;
+}
+
+/* QuickTime transformation matrix.
+a,b,c,d,x,y: divided as 16.16 bits.
+u,v,w;       divided as 2.30 bits
+// REF: https://github.com/sannies/mp4parser/blob/master/isoparser/src/main/java/org/mp4parser/support/Matrix.java */
+struct matrix {
+    static constexpr uint32_t SIZE = 9 * sizeof(int32_t); // serialization size
+    double a, b, u;
+    double c, d, v;
+    double tx, ty, w;
+
+    matrix(const char* buf) {
+        // TOOD: Implement fixed-point parsing
+        a = ReadFixed1616(buf); buf += sizeof(int32_t);
+        b = ReadFixed1616(buf); buf += sizeof(int32_t);
+        u = ReadFixed0230(buf); buf += sizeof(int32_t);
+        c = ReadFixed1616(buf); buf += sizeof(int32_t);
+        d = ReadFixed1616(buf); buf += sizeof(int32_t);
+        v = ReadFixed0230(buf); buf += sizeof(int32_t);
+        tx = ReadFixed1616(buf); buf += sizeof(int32_t);
+        ty = ReadFixed1616(buf); buf += sizeof(int32_t);
+        w = ReadFixed0230(buf); buf += sizeof(int32_t);
+    }
+
+    char* Write(char* buf) const {
+        buf = WriteFixed1616(buf, a);
+        buf = WriteFixed1616(buf, b);
+        buf = WriteFixed0230(buf, u);
+        buf = WriteFixed1616(buf, c);
+        buf = WriteFixed1616(buf, d);
+        buf = WriteFixed0230(buf, v);
+        buf = WriteFixed1616(buf, tx);
+        buf = WriteFixed1616(buf, ty);
+        buf = WriteFixed0230(buf, w);
+        return buf;
+    }
+};
+
+/**  MP4 file uses time counting in SECONDS since midnight, Jan. 1, 1904. */
+inline uint64_t CurrentTime1904() {
+#if 0
+    FILETIME curTime{};
+    {
+        SYSTEMTIME st{};
+        GetSystemTime(&st);
+        SystemTimeToFileTime(&st, &curTime);
+    }
+
+    FILETIME epochTime{};
+    {
+        // unix 1970 epoch
+        SYSTEMTIME st{};
+        st.wYear = 1904;
+        st.wMonth = 1;
+        st.wDay = 1;
+        // for some reason needed to adjust epoch by 7min 10sec to match time(null)
+        st.wHour = 0;
+        st.wMinute = 7;
+        st.wSecond = 10;
+        SystemTimeToFileTime(&st, &epochTime);
+    }
+
+    ULARGE_INTEGER diff{}; // 100ns resolution
+    diff.HighPart = curTime.dwHighDateTime - epochTime.dwHighDateTime;
+    diff.LowPart = curTime.dwLowDateTime - epochTime.dwLowDateTime;
+    return diff.QuadPart / 10000000;
+#else
+    time_t now = time(NULL); // unix epoch since 1970-01-01
+    // Convert from unix epoch to MPEG-4 epoch since midnight, Jan. 1, 1904.
+    // Seconds between 1904-01-01 and Unix 1970 Epoch: (66 * 365 + 17) * (24 * 60 * 60) = 2082844800 (from https://github.com/FFmpeg/FFmpeg/blob/master/libavformat/mov.c)
+    uint64_t mpeg4Time = now + (66 * 365 + 17) * (24 * 60 * 60);
+    return mpeg4Time;
+#endif
+}
