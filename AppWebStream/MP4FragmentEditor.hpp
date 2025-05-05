ba445296e473c3add4dfacbfb1b9dfdf76b189b5
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-05-05 14:43:08 +0200

    .

--- AppWebStream/MP4FragmentEditor.hpp
+++ AppWebStream/MP4FragmentEditor.hpp
@@ -70,8 +70,14 @@ public:
                 return ModifyMovieFragment(buffer.data(), atom_size);
             else
                 return buffer;
+        } else if (IsAtomType(buffer.data(), "mdat")) {
+            uint32_t atom_size = GetAtomSize(buffer.data());
+            // don't check buffer size, since the encoded pixel payload arrives in a later call
+
+            // Media Data (mdat) - only header data
+            return buffer;
         } else {
-            // leave other atoms unchanged
+            // leave other atoms and encoded pixels unchanged
             return buffer;
         }
     }
