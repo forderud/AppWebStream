7c1261e536330122d1eb7906863c9e3538480c05
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-05-08 19:35:55 +0200

    Add InputStream skeleton class.

---
+++ StreamReceiver/InputStream.cpp
@@ -0,0 +1,72 @@
+#define WIN32_LEAN_AND_MEAN
+#include <comdef.h> // for _com_error
+#include <Mfapi.h>
+#include "InputStream.hpp"
+
+
+InputStream::InputStream() {
+}
+
+InputStream::~InputStream() {
+}
+
+HRESULT InputStream::GetCapabilities(/*out*/DWORD *capabilities) {
+    *capabilities = MFBYTESTREAM_IS_READABLE | MFBYTESTREAM_IS_REMOTE;
+    return S_OK;
+}
+
+HRESULT InputStream::GetLength(/*out*/QWORD* /*length*/) {
+    return E_NOTIMPL;
+}
+
+HRESULT InputStream::SetLength(/*in*/QWORD /*length*/) {
+    return E_NOTIMPL;
+}
+
+HRESULT InputStream::GetCurrentPosition(/*out*/QWORD* /*position*/) {
+    return E_NOTIMPL;
+}
+
+HRESULT InputStream::SetCurrentPosition(/*in*/QWORD /*position*/) {
+    return E_NOTIMPL;
+}
+
+HRESULT InputStream::IsEndOfStream(/*out*/BOOL* /*endOfStream*/) {
+    return E_NOTIMPL;
+}
+
+HRESULT InputStream::Read(/*out*/BYTE* /*pb*/, /*in*/ULONG /*cb*/, /*out*/ULONG* /*bRead*/) {
+    return E_NOTIMPL;
+}
+
+HRESULT InputStream::BeginRead(/*out*/BYTE* /*pb*/, /*in*/ULONG /*cb*/, /*in*/IMFAsyncCallback* /*callback*/, /*in*/IUnknown* /*unkState*/) {
+    return E_NOTIMPL;
+}
+
+HRESULT InputStream::EndRead(/*in*/IMFAsyncResult* /*result*/, /*out*/ULONG* /*cbRead*/) {
+    return E_NOTIMPL;
+}
+
+HRESULT InputStream::Write(/*in*/const BYTE* /*pb*/, /*in*/ULONG /*cb*/, /*out*/ULONG* /*cbWritten*/) {
+    return E_NOTIMPL;
+}
+
+HRESULT InputStream::BeginWrite(/*in*/const BYTE* /*pb*/, /*in*/ULONG /*cb*/, /*in*/IMFAsyncCallback* /*callback*/, /*in*/IUnknown* /*unkState*/) {
+    return E_NOTIMPL;
+}
+
+HRESULT InputStream::EndWrite(/*in*/IMFAsyncResult* /*result*/, /*out*/ULONG* /*cbWritten*/) {
+    return E_NOTIMPL;
+}
+
+HRESULT InputStream::Seek(/*in*/MFBYTESTREAM_SEEK_ORIGIN /*SeekOrigin*/, /*in*/LONGLONG /*SeekOffset*/,/*in*/DWORD /*SeekFlags*/, /*out*/QWORD* /*CurrentPosition*/) {
+    return E_NOTIMPL;
+}
+
+HRESULT InputStream::Flush() {
+    return E_NOTIMPL;
+}
+
+HRESULT InputStream::Close() {
+    return E_NOTIMPL;
+}
---
+++ StreamReceiver/InputStream.hpp
@@ -0,0 +1,51 @@
+#pragma once
+#include <atlbase.h>
+#include <atlcom.h>
+#include <MFidl.h>
+#include <Mfreadwrite.h>
+
+
+class ATL_NO_VTABLE InputStream :
+    public CComObjectRootEx<CComMultiThreadModel>,
+    public CComCoClass<InputStream>,
+    public IMFByteStream {
+public:
+    InputStream();
+    /*NOT virtual*/ ~InputStream();
+
+    HRESULT GetCapabilities(/*out*/DWORD *capabilities) override;
+
+    HRESULT GetLength(/*out*/QWORD* length) override;
+
+    HRESULT SetLength(/*in*/QWORD length) override;
+
+    HRESULT GetCurrentPosition(/*out*/QWORD* position) override;
+
+    HRESULT SetCurrentPosition(/*in*/QWORD position) override;
+
+    HRESULT IsEndOfStream(/*out*/BOOL* endOfStream) override;
+
+    HRESULT Read(/*out*/BYTE* pb, /*in*/ULONG cb, /*out*/ULONG* bRead) override;
+
+    HRESULT BeginRead(/*out*/BYTE* pb, /*in*/ULONG cb, /*in*/IMFAsyncCallback* callback, /*in*/IUnknown* unkState) override;
+
+    HRESULT EndRead(/*in*/IMFAsyncResult* result, /*out*/ULONG* cbRead) override;
+
+    HRESULT Write(/*in*/const BYTE* pb, /*in*/ULONG cb, /*out*/ULONG* cbWritten) override;
+
+    HRESULT BeginWrite(/*in*/const BYTE* pb, /*in*/ULONG cb, /*in*/IMFAsyncCallback* callback, /*in*/IUnknown* unkState) override;
+
+    HRESULT EndWrite(/*in*/IMFAsyncResult* result, /*out*/ULONG* cbWritten) override;
+
+    HRESULT Seek(/*in*/MFBYTESTREAM_SEEK_ORIGIN SeekOrigin, /*in*/LONGLONG SeekOffset,/*in*/DWORD SeekFlags, /*out*/QWORD* CurrentPosition) override;
+
+    HRESULT Flush() override;
+
+    HRESULT Close() override;
+
+    BEGIN_COM_MAP(InputStream)
+        COM_INTERFACE_ENTRY(IMFByteStream)
+    END_COM_MAP()
+
+private:
+};
