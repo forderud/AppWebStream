4cb54c96d378365268292ad1dae207441c619bf8
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-05-08 12:26:23 +0200

    Start with skeleton impl.

---
+++ StreamReceiver/Main.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string>
+#include <stdexcept>
+#include <windows.h>
+#include <comdef.h> // for _com_error
+#include <mfapi.h>
+#include <mfidl.h>
+#include <mfreadwrite.h>
+
+
+#pragma comment(lib, "Mfplat.lib")
+#pragma comment(lib, "Mfreadwrite.lib")
+
+
+/** Converts unicode string to ASCII */
+static inline std::string ToAscii(const std::wstring& w_str) {
+#pragma warning(push)
+#pragma warning(disable: 4996) // function or variable may be unsafe
+    size_t N = w_str.size();
+    std::string s_str;
+    s_str.resize(N);
+    wcstombs(const_cast<char*>(s_str.data()), w_str.c_str(), N);
+
+    return s_str;
+#pragma warning(pop)
+}
+
+static void COM_CHECK(HRESULT hr) {
+    if (FAILED(hr)) {
+        _com_error err(hr);
+        const wchar_t* msg = err.ErrorMessage(); // weak ptr.
+        throw std::runtime_error(ToAscii(msg));
+    }
+}
+
+// define smart-pointers with "Ptr" suffix
+_COM_SMARTPTR_TYPEDEF(IMFAttributes, __uuidof(IMFAttributes));
+_COM_SMARTPTR_TYPEDEF(IMFSourceReader, __uuidof(IMFSourceReader));
+
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        printf("Usage: StreamReceiver.exe <host>:<port> (e.g. StreamReceiver.exe localhost:8080)\n");
+        return -1;
+    }
+
+    char* host_port = argv[1];
+
+    COM_CHECK(MFStartup(MF_VERSION));
+
+    _bstr_t url = L"http://";
+    url += host_port;
+
+    // TODO: Connect to MPEG4 H.264 stream
+    IMFAttributesPtr attribs = nullptr;
+    IMFSourceReaderPtr reader;
+    COM_CHECK(MFCreateSourceReaderFromURL(url, attribs, &reader));
+}
--- StreamReceiver/StreamReceiver.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "TODO: Implement MPEG4 H.264 receive.\n";
-}
