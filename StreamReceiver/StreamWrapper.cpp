3a13756c2f409ba892ceb7ca333068c5f8c4e109
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-05-09 15:43:15 +0200

    .

---
+++ StreamReceiver/StreamWrapper.cpp
@@ -0,0 +1,78 @@
+#define WIN32_LEAN_AND_MEAN
+#include <stdexcept>
+#include <tuple>
+#include <comdef.h> // for _com_error
+#include <Mfapi.h>
+#include "StreamWrapper.hpp"
+
+
+StreamWrapper::StreamWrapper() {
+}
+
+StreamWrapper::~StreamWrapper() {
+}
+
+void StreamWrapper::Initialize(IMFByteStream* obj) {
+    m_obj = obj;
+}
+
+
+HRESULT StreamWrapper::GetCapabilities(/*out*/DWORD *capabilities) {
+    return m_obj->GetCapabilities(capabilities);
+}
+
+HRESULT StreamWrapper::GetLength(/*out*/QWORD* length) {
+    return m_obj->GetLength(length);
+}
+
+HRESULT StreamWrapper::SetLength(/*in*/QWORD length) {
+    return m_obj->SetLength(length);
+}
+
+HRESULT StreamWrapper::GetCurrentPosition(/*out*/QWORD* position) {
+    return m_obj->GetCurrentPosition(position);
+}
+
+HRESULT StreamWrapper::SetCurrentPosition(/*in*/QWORD position) {
+    return m_obj->SetCurrentPosition(position);
+}
+
+HRESULT StreamWrapper::IsEndOfStream(/*out*/BOOL* endOfStream) {
+    return m_obj->IsEndOfStream(endOfStream);
+}
+
+HRESULT StreamWrapper::Read(/*out*/BYTE* pb, /*in*/ULONG cb, /*out*/ULONG* bRead) {
+    return m_obj->Read(pb, cb, bRead);
+}
+
+HRESULT StreamWrapper::BeginRead(/*out*/BYTE* pb, /*in*/ULONG cb, /*in*/IMFAsyncCallback* callback, /*in*/IUnknown* unkState) {
+    return m_obj->BeginRead(pb, cb, callback, unkState);
+}
+
+HRESULT StreamWrapper::EndRead(/*in*/IMFAsyncResult* result, /*out*/ULONG* cbRead) {
+    return m_obj->EndRead(result, cbRead);
+}
+
+HRESULT StreamWrapper::Write(/*in*/const BYTE* pb, /*in*/ULONG cb, /*out*/ULONG* cbWritten) {
+    return m_obj->Write(pb, cb, cbWritten);
+}
+
+HRESULT StreamWrapper::BeginWrite(/*in*/const BYTE* pb, /*in*/ULONG cb, /*in*/IMFAsyncCallback* callback, /*in*/IUnknown* unkState) {
+    return m_obj->BeginWrite(pb, cb, callback, unkState);
+}
+
+HRESULT StreamWrapper::EndWrite(/*in*/IMFAsyncResult* result, /*out*/ULONG* cbWritten) {
+    return m_obj->EndWrite(result, cbWritten);
+}
+
+HRESULT StreamWrapper::Seek(/*in*/MFBYTESTREAM_SEEK_ORIGIN SeekOrigin, /*in*/LONGLONG SeekOffset,/*in*/DWORD SeekFlags, /*out*/QWORD* CurrentPosition) {
+    return m_obj->Seek(SeekOrigin, SeekOffset, SeekFlags, CurrentPosition);
+}
+
+HRESULT StreamWrapper::Flush() {
+    return m_obj->Flush();
+}
+
+HRESULT StreamWrapper::Close() {
+    return m_obj->Close();
+}
---
+++ StreamReceiver/StreamWrapper.hpp
@@ -0,0 +1,57 @@
+#pragma once
+#include <memory>
+#include <atlbase.h>
+#include <atlcom.h>
+#include <MFidl.h>
+#include <Mfreadwrite.h>
+
+_COM_SMARTPTR_TYPEDEF(IMFByteStream, __uuidof(IMFByteStream));
+
+
+class ATL_NO_VTABLE StreamWrapper :
+    public CComObjectRootEx<CComMultiThreadModel>,
+    public CComCoClass<StreamWrapper>,
+    public IMFByteStream {
+public:
+    StreamWrapper();
+    /*NOT virtual*/ ~StreamWrapper();
+
+    void Initialize(IMFByteStream * obj);
+
+    HRESULT GetCapabilities(/*out*/DWORD *capabilities) override;
+
+    HRESULT GetLength(/*out*/QWORD* length) override;
+
+    HRESULT SetLength(/*in*/QWORD length) override;
+
+    HRESULT GetCurrentPosition(/*out*/QWORD* position) override;
+
+    HRESULT SetCurrentPosition(/*in*/QWORD position) override;
+
+    HRESULT IsEndOfStream(/*out*/BOOL* endOfStream) override;
+
+    HRESULT Read(/*out*/BYTE* pb, /*in*/ULONG cb, /*out*/ULONG* bRead) override;
+
+    HRESULT BeginRead(/*out*/BYTE* pb, /*in*/ULONG cb, /*in*/IMFAsyncCallback* callback, /*in*/IUnknown* unkState) override;
+
+    HRESULT EndRead(/*in*/IMFAsyncResult* result, /*out*/ULONG* cbRead) override;
+
+    HRESULT Write(/*in*/const BYTE* pb, /*in*/ULONG cb, /*out*/ULONG* cbWritten) override;
+
+    HRESULT BeginWrite(/*in*/const BYTE* pb, /*in*/ULONG cb, /*in*/IMFAsyncCallback* callback, /*in*/IUnknown* unkState) override;
+
+    HRESULT EndWrite(/*in*/IMFAsyncResult* result, /*out*/ULONG* cbWritten) override;
+
+    HRESULT Seek(/*in*/MFBYTESTREAM_SEEK_ORIGIN SeekOrigin, /*in*/LONGLONG SeekOffset,/*in*/DWORD SeekFlags, /*out*/QWORD* CurrentPosition) override;
+
+    HRESULT Flush() override;
+
+    HRESULT Close() override;
+
+    BEGIN_COM_MAP(StreamWrapper)
+        COM_INTERFACE_ENTRY(IMFByteStream)
+    END_COM_MAP()
+
+private:
+    IMFByteStreamPtr m_obj;
+};
