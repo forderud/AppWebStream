65f4117c848c0d7db8bdeb7d0612afe6063d3db0
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-05-12 15:24:54 +0200

    Move screen capture logic to a separate header.

--- AppWebStream/Main.cpp
+++ AppWebStream/Main.cpp
@@ -3,64 +3,8 @@
 #include <string>
 #include "VideoEncoder.hpp"
 #include "OutputStream.hpp"
+#include "ScreenCapture.hpp"
 
-
-class window_dc {
-public:
-    window_dc(HWND h) : wnd(h) {
-        dc = GetDC(wnd);
-        if (wnd) {
-            if (!GetClientRect(h, &rect))
-                throw std::runtime_error("GetClientRect failed");
-        } else {
-            // primary monitor resolution
-            rect.right  = GetSystemMetrics(SM_CXSCREEN);
-            rect.bottom = GetSystemMetrics(SM_CYSCREEN);
-        }
-    }
-    ~window_dc() {
-        ReleaseDC(wnd, dc);
-    }
-
-    LONG width() const {
-        return rect.right - rect.left;
-    }
-    LONG height() const {
-        return rect.bottom - rect.top;
-    }
-
-    HWND wnd  = nullptr;
-    HDC  dc   = nullptr;
-private:
-    RECT rect = {};
-};
-
-
-class offscreen_bmp {
-public:
-    offscreen_bmp(HDC ref_dc, LONG width, LONG height) {
-        dc = CreateCompatibleDC(ref_dc);
-        if (!dc)
-            throw std::runtime_error("CreateCompatibleDC has failed");
-
-        bmp = CreateCompatibleBitmap(ref_dc, width, height);
-        if (!bmp)
-            throw std::runtime_error("CreateCompatibleBitmap Failed");
-
-        // make bitmap current for dc
-        prev = SelectObject(dc, bmp);
-    }
-    ~offscreen_bmp() {
-        SelectObject(dc, prev);
-        DeleteObject(bmp);
-        DeleteDC(dc);
-    }
-
-    HDC     dc   = nullptr;
-    HBITMAP bmp  = nullptr;
-private:
-    HGDIOBJ prev = nullptr;
-};
 
 static HRESULT EncodeFrame (VideoEncoder & encoder, window_dc & wnd_dc) {
     // create offscreen bitmap with device context
