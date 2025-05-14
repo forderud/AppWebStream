1162ec7ede9391a93a56492e8b42972a1c43dc77
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-05-14 12:10:10 +0200

    .

--- StreamReceiver/Mpeg4Receiver.hpp
+++ StreamReceiver/Mpeg4Receiver.hpp
@@ -16,13 +16,12 @@ public:
     virtual void OnStartTimeDpiChanged(uint64_t startTime, double dpi) = 0;
 };
 
-class Mpeg4Receiver; // forward decl.
-
-/** frameTime is in 100-nanosecond units since startTime. frameDuration is also in 100-nanosecond units. */
-typedef std::function<void(Mpeg4Receiver& receiver, int64_t frameTime, int64_t frameDuration, std::string_view buffer, bool metadataChanged)> ProcessFrameCb;
 
 class Mpeg4Receiver : public IStartTimeDPIReceiver {
 public:
+    /** frameTime is in 100-nanosecond units since startTime. frameDuration is also in 100-nanosecond units. */
+    typedef std::function<void(Mpeg4Receiver& receiver, int64_t frameTime, int64_t frameDuration, std::string_view buffer, bool metadataChanged)> ProcessFrameCb;
+
     /** Connect to requested MPEG4 URL. The "frame_cb" callback will be called from the same thread for every frame received. */
     Mpeg4Receiver(_bstr_t url, ProcessFrameCb frame_cb);
 
