9928b9e604f4ee27f1049d49405dc837851800f2
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-05-14 12:48:18 +0200

    Update doc.

--- StreamReceiver/Mpeg4Receiver.hpp
+++ StreamReceiver/Mpeg4Receiver.hpp
@@ -7,17 +7,19 @@
 
 struct IMFSourceReader; // forward decl.
 
-
+/** Receiver for fragmented MPEG4 streams over a network.
+    Does internally use the Media Foundation API, but that can change in the future. */
 class Mpeg4Receiver {
 public:
     /** frameTime is in 100-nanosecond units since startTime. frameDuration is also in 100-nanosecond units. */
     typedef std::function<void(Mpeg4Receiver& receiver, int64_t frameTime, int64_t frameDuration, std::string_view buffer, bool metadataChanged)> ProcessFrameCb;
 
-    /** Connect to requested MPEG4 URL. The "frame_cb" callback will be called from the same thread for every frame received. */
+    /** Connect to requested MPEG4 URL. */
     Mpeg4Receiver(_bstr_t url, ProcessFrameCb frame_cb);
 
     ~Mpeg4Receiver();
 
+    /** Receive frames. The "frame_cb" callback will be called from the same thread when new frames are received. */
     HRESULT ReceiveFrame();
 
     uint64_t GetStartTime() const {
