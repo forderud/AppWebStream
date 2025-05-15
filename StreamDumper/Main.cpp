de7a6fb85e4e49fc246fbef1d844a2aa05b9ea4d
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-05-15 14:14:35 +0200

    .

--- StreamDumper/Main.cpp
+++ StreamDumper/Main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include  <comutil.h>
+
+#pragma comment(lib, "comsuppw.lib")
+
 
 int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        wprintf(L"Usage: StreamReceiver.exe URL (e.g. StreamReceiver.exe http://localhost:8080/movie.mp4)\n");
+        return -1;
+    }
+
+    _bstr_t url = argv[1];
     printf("TODO: Write tool for dumping network stream sessions to file.\n");
 }
