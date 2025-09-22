494bb06049b430ffb5bd61327cafacdf622bfdc9
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-09-22 17:56:45 +0200

    .

--- StreamReceiver/Mpeg4Receiver.hpp
+++ StreamReceiver/Mpeg4Receiver.hpp
@@ -44,7 +44,7 @@ private:
     void OnStartTimeDpiChanged(uint64_t startTime, double dpi, double xform[6]);
     HRESULT ConfigureOutputType(IMFSourceReader& reader, DWORD dwStreamIndex);
 
-    CComPtr<IMFSourceReader> m_reader = nullptr;
+    CComPtr<IMFSourceReader> m_reader;
     uint64_t                 m_startTime = 0;   // SECONDS since midnight, Jan. 1, 1904
     double                   m_dpi = 0;         // pixel spacing
     double                   m_xform[6] = { 1, 0, 0, 1, 0, 0 }; // initialize with default identity transform
