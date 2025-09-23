a2d9d2e3850a8c2077756218ce3c09a0bc68a241
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-09-23 08:54:15 +0200

    Move GetResolution impl to hpp.

--- StreamReceiver/Mpeg4ReceiverSR.cpp
+++ StreamReceiver/Mpeg4ReceiverSR.cpp
@@ -26,13 +26,6 @@ _COM_SMARTPTR_TYPEDEF(IPropertyStore, __uuidof(IPropertyStore));
 _COM_SMARTPTR_TYPEDEF(IMFMediaSource, __uuidof(IMFMediaSource));
 
 
-static unsigned int Align16(unsigned int size) {
-    if ((size % 16) == 0)
-        return size;
-    else
-        return size + 16 - (size % 16);
-}
-
 Mpeg4ReceiverSR::Mpeg4ReceiverSR(_bstr_t url, NewFrameCb frame_cb) : m_frame_cb(frame_cb) {
     m_resolution.fill(0); // clear array
 
@@ -213,14 +206,6 @@ HRESULT Mpeg4ReceiverSR::ReceiveFrame() {
     }
 
     return S_OK;
-}
-
-std::array<uint32_t, 2> Mpeg4ReceiverSR::GetResolution() const {
-    // return resolution of output buffer, that's a multiple of MPEG4 16x16 macroblocks 
-    std::array<uint32_t, 2> result;
-    result[0] = Align16(m_resolution[0]);
-    result[1] = Align16(m_resolution[1]);
-    return result;
 }
 
 void Mpeg4ReceiverSR::OnStartTimeDpiChanged(uint64_t startTime, double dpi, double xform[6]) {
--- StreamReceiver/Mpeg4ReceiverSR.hpp
+++ StreamReceiver/Mpeg4ReceiverSR.hpp
@@ -7,6 +7,13 @@
 
 struct IMFSourceReader; // forward decl.
 
+static unsigned int Align16(unsigned int size) {
+    if ((size % 16) == 0)
+        return size;
+    else
+        return size + 16 - (size % 16);
+}
+
 /** Receiver for fragmented MPEG4 streams over a network.
     Does internally use the Media Foundation API, but that can change in the future. */
 class Mpeg4ReceiverSR {
@@ -41,7 +48,13 @@ public:
             xform[i] = m_xform[i];
     }
 
-    std::array<uint32_t, 2> GetResolution() const;
+    std::array<uint32_t, 2> GetResolution() const {
+        // return resolution of output buffer, that's a multiple of MPEG4 16x16 macroblocks 
+        std::array<uint32_t, 2> result;
+        result[0] = Align16(m_resolution[0]);
+        result[1] = Align16(m_resolution[1]);
+        return result;
+    }
 
 private:
     void OnStartTimeDpiChanged(uint64_t startTime, double dpi, double xform[6]);
