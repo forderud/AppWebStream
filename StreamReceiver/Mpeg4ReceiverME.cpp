f56a2a2bf670a184dcdf484260261fac4f2b2053
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-09-23 14:51:16 +0200

    working baseline with on-screen image display :-)

--- StreamReceiver/Mpeg4ReceiverME.cpp
+++ StreamReceiver/Mpeg4ReceiverME.cpp
@@ -179,7 +179,20 @@ void Mpeg4ReceiverME::OnFrameArrived() {
     if (FAILED(hr))
         throw std::runtime_error("TransferVideoFrame failed");
 
-    std::string_view buffer; // TODO: Extract RGB pixels from bitmap
+    std::string_view buffer;
+    CComPtr<IWICBitmapLock> lock;
+    {
+        WICRect rect = { 0, 0, (INT)m_resolution[0], (INT)m_resolution[1]};
+        hr = m_bitmap->Lock(&rect, WICBitmapLockRead, &lock);
+        assert(SUCCEEDED(hr));
+
+        UINT size = 0;
+        WICInProcPointer ptr = nullptr;
+        hr = lock->GetDataPointer(&size, &ptr);
+        assert(SUCCEEDED(hr));
+
+        buffer = std::string_view((char*)ptr, size);
+    }
     m_frame_cb(*this, time_100ns, duration_100ns, buffer, m_metadata_changed);
 
     m_metadata_changed = false; // clear flag after m_frame_cb have been called
