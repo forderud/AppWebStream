e62ba1ba0abca93d6d07e3a8e365c0a5811f306c
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-09-23 09:46:53 +0200

    ease testing

--- StreamReceiver/Mpeg4Receiver.cpp
+++ StreamReceiver/Mpeg4Receiver.cpp
@@ -1,7 +1,12 @@
 #pragma once
+#include "Mpeg4ReceiverME.hpp"
 #include "Mpeg4ReceiverSR.hpp"
 
 
 std::unique_ptr<Mpeg4Receiver> Mpeg4Receiver::Create(_bstr_t url, NewFrameCb frame_cb) {
+#if 0
+    return std::make_unique<Mpeg4ReceiverME>(url, frame_cb);
+#else
     return std::make_unique<Mpeg4ReceiverSR>(url, frame_cb);
+#endif
 }
